35aa80ce3077c78552c9838ae440eaf6524f4961
Sebastian Vollnhals <sebastian@vollnhals.info>
2012-04-26 12:51:58 +0200

    changed text

--- catalog/includes/languages/english/modules/shipping/tiramizoo.php
+++ catalog/includes/languages/english/modules/shipping/tiramizoo.php
@@ -3,7 +3,7 @@
 define('MODULE_SHIPPING_TIRAMIZOO_TEXT_TITLE', 'Tiramizoo');
 define('MODULE_SHIPPING_TIRAMIZOO_TEXT_DESCRIPTION', 'Shipping with bike messenger');
 
-define('MODULE_SHIPPING_TIRAMIZOO_TEXT_QUOTE_TITLE', 'Shipping with bike messenger');
+define('MODULE_SHIPPING_TIRAMIZOO_TEXT_QUOTE_TITLE', 'Shipping by tiramizoo<br />');
 define('MODULE_SHIPPING_TIRAMIZOO_TEXT_SUBMIT_ERROR_TITLE', 'Booking of Tiramizoo shipping failed');
 define('MODULE_SHIPPING_TIRAMIZOO_TEXT_SUBMIT_ERROR_CONTENT', 'osCommerce could not transmit the shipping data to Tiramizoo for order #%s. Please contact Tiramizoo support.');
 
